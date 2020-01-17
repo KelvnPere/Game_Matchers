c442a0d19fa89a0c74e22534c450add579bcfc84
Pheonix73 <codebear73@gmail.com>
2020-01-17 02:16:14 +0100

    Add some resources pictures and built the memory matching game.

--- app/src/main/java/word/searchg/gamematchers/MainActivity.kt
+++ app/src/main/java/word/searchg/gamematchers/MainActivity.kt
@@ -1,39 +1,48 @@
 package word.searchg.gamematchers
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
+
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import word.searchg.gamematchers.matchers.FoodMatcherActivity
+import word.searchg.gamematchers.matchers.PlaceMatcherActivity
+import word.searchg.gamematchers.matchers.ShoeMatcherActivity
 
 class MainActivity : AppCompatActivity() {
+    val  tag  = "ClickListener"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
+        foodMatcherBtn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this,FoodMatcherActivity::class.java)
+            Log.d(tag, "FoodMatcher Called")
+            startActivity(intent)
+        })
+
+        placeMatcherBtn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this,PlaceMatcherActivity::class.java)
+            Log.d(tag, "PlaceMatcher Called")
+            startActivity(intent)
+        })
+
+        shoeMatcherBtn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this,ShoeMatcherActivity::class.java)
+            Log.d(tag, "ShoeMatcher Called")
+            startActivity(intent)
+        })
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
+
+
+
 }
--- app/src/main/java/word/searchg/gamematchers/matchers/FoodMatcherActivity.kt
+++ app/src/main/java/word/searchg/gamematchers/matchers/FoodMatcherActivity.kt
@@ -1,11 +1,14 @@
 package word.searchg.gamematchers.matchers
 
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import word.searchg.gamematchers.R
 
 import kotlinx.android.synthetic.main.activity_food_matcher.*
+import kotlinx.android.synthetic.main.content_food_matcher.*
+import word.searchg.gamematchers.R.drawable.*
 
 class FoodMatcherActivity : AppCompatActivity() {
 
@@ -14,9 +17,65 @@ class FoodMatcherActivity : AppCompatActivity() {
         setContentView(R.layout.activity_food_matcher)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        // images to be shown
+        val images: MutableList<Int> =
+            mutableListOf(banana, beans, bole, plantain, rice, soup,banana, beans, bole, plantain, rice, soup)
+
+        // button associated with the images
+        val buttons: Array<Button> = arrayOf(
+            button1,
+            button2,
+            button3,
+            button4,
+            button5,
+            button6,
+            button7,
+            button8,
+            button9,
+            button10,
+            button11,
+            button12
+        )
+
+        val cardBack = star
+        var clicked = 0
+        var turnOver = false
+        var lastClicked = -1
+
+        // this will randomlize the placement of the images in the button.
+        images.shuffle()
+
+        for (i in 0..11) {
+           // buttons[i].setBackgroundResource(plantain)
+            buttons[i].text = "backOfTheCard"
+           // buttons[i].textSize = 0.0F
+            buttons[i].setOnClickListener {
+                if (buttons[1].text == "backOfTheCard" && !turnOver) {
+                    buttons[i].setBackgroundResource(images[i])
+                    buttons[i].setText(images[i])
+                    if (clicked == 0){
+                        lastClicked = i
+                    }
+                    clicked++
+                }else if (buttons[i].text !in "backOfTheCard"){
+                    buttons[i].setBackgroundResource(cardBack)
+                    buttons[i].text = "backOfTheCard"
+                    clicked--
+                }
+
+            if (clicked == 2){
+                turnOver = true
+               if (buttons[i].text == buttons[lastClicked].text){
+                   buttons[i].isClickable = false
+                   buttons[lastClicked].isClickable = false
+                     turnOver = false
+                      clicked = 0
+               }
+            }else if (clicked == 0){
+                turnOver = false
+            }
+
+            }
         }
     }
 
